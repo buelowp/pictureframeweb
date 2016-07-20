ce44cc179e5c9e7c75edeb18356f3a167aabf8ac
Peter Buelow <peter.buelow@lifefitness.com>
2016-07-20 09:43:04 -0500

    Updated styling
    Fixed some language issues
    Fixed styling for the Index page

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html>
+<head>
+ <link rel="stylesheet" type="text/css" href="localstyle.css">
+</head>
 <title>Picture Frame Manager</title>
 <body>
 
 <h1>Manage Picture Frame contents</h1>
-</h2>Select the frame, and manage the files that will be displayed.</h2>
+<h2>Select the frame, and manage the files that will be displayed.</h2>
 
 <form action="uploadfor.php" method="post">
 <h2>Select who to upload images for</h2>
-<select name="user">
- <option value="pete">Pete</option>
- <option value="dawn">Dawn</option>
-</select>
-<br>
-<input type="submit" value="Select User" name="submit">
+<table id="select-form">
+ <tr id=select-form">
+  <td id=select-form">
+   <select name="user" id="user-select">
+    <option value="pete">Pete</option>
+    <option value="dawn">Dawn</option>
+   </select>
+  </td>
+  <td id="select-form"><input id="usersubmit" type="submit" value="Select User" name="submit"></td>
+ </tr>
+</table>
 </form> 
 
 </body>
--- uploadfor.php
+++ uploadfor.php
@@ -8,8 +8,9 @@
 <table class="upload">
  <tr>
   <td class="upload">
-	<h1>Select a file to upload for <?php printf("%s", $_POST['user']); ?>'s picture frame</h1>
+	<h1>Select the files to be displayed on <?php printf("%s", $_POST['user']); ?>'s picture frame</h1>
 	<h2>Must be a .jpg image for now. Should be sized to 1280x800 if possible.</h2>
+	<h2>You can add new files here, or delete selected files.</h2>
 	<form action="picuploader.php" method="post" enctype="multipart/form-data">
 	<table class="uploadform">
 	 <tr class="uploadform">
